305b867cfbceb0bed2b95702e4fd4d88c0301f0e
Fernando del Caz <fernandodelcaz@gmail.com>
2024-05-24 17:15:49 +0200

    chore: add setup function

--- java/src/test/java/BowlingKataTest.java
+++ java/src/test/java/BowlingKataTest.java
@@ -1,14 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingKataTest {
 
+  private BowlingGame bowlingGame;
+
+  @BeforeEach
+  public void setup() {
+    this.bowlingGame = new BowlingGame();
+  }
+
   @Test
   public void all_spares_should_count_zero_points() {
 
     String input = "-- -- -- -- -- -- -- -- -- -- ";
-    BowlingGame bowlingGame = new BowlingGame();
 
     assertEquals(0, bowlingGame.getScore(input));
   }
@@ -17,7 +24,6 @@ public class BowlingKataTest {
   public void one_two_three_should_count_six_points() {
 
     String input = "12 3- -- -- -- -- -- -- -- -- ";
-    BowlingGame bowlingGame = new BowlingGame();
 
     assertEquals( 6, bowlingGame.getScore(input));
   }
@@ -25,7 +31,6 @@ public class BowlingKataTest {
   @Test
   public void ten_pairs_of_nine_and_a_miss_should_count_ninety_points() {
     String input = "9- 9- 9- 9- 9- 9- 9- 9- 9- 9-";
-    BowlingGame bowlingGame = new BowlingGame();
 
     assertEquals( 90, bowlingGame.getScore(input));
   }
