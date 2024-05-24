bf31e6d71a92fb66bb3c1d2c0a1a5cbb594fefdf
Fernando del Caz <fernandodelcaz@gmail.com>
2024-05-24 17:18:56 +0200

    Chore: Parameterize test

--- java/src/test/java/BowlingKataTest.java
+++ java/src/test/java/BowlingKataTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,25 +14,23 @@ public class BowlingKataTest {
     this.bowlingGame = new BowlingGame();
   }
 
-  @Test
-  public void all_spares_should_count_zero_points() {
-
-    String input = "-- -- -- -- -- -- -- -- -- -- ";
+  @ParameterizedTest
+  @ValueSource(strings = {"-- -- -- -- -- -- -- -- -- -- "})
+  public void all_spares_should_count_zero_points(String input) {
 
     assertEquals(0, bowlingGame.getScore(input));
   }
 
-  @Test
-  public void one_two_three_should_count_six_points() {
-
-    String input = "12 3- -- -- -- -- -- -- -- -- ";
+  @ParameterizedTest
+  @ValueSource(strings = {"12 3- -- -- -- -- -- -- -- -- "})
+  public void one_two_three_should_count_six_points(String input) {
 
     assertEquals( 6, bowlingGame.getScore(input));
   }
 
-  @Test
-  public void ten_pairs_of_nine_and_a_miss_should_count_ninety_points() {
-    String input = "9- 9- 9- 9- 9- 9- 9- 9- 9- 9-";
+  @ParameterizedTest
+  @ValueSource(strings = {"9- 9- 9- 9- 9- 9- 9- 9- 9- 9-"})
+  public void ten_pairs_of_nine_and_a_miss_should_count_ninety_points(String input) {
 
     assertEquals( 90, bowlingGame.getScore(input));
   }
